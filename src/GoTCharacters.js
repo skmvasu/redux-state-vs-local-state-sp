db124cbdd6a837ed69df9bfa541b65ceea0b8425
Vasu K <vasu@mangosense.com>
2017-09-06 22:13:44 +0530

    Edit character form

--- src/GoTCharacters.js
+++ src/GoTCharacters.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const GoTCharacters = ({characters,toggleCharacterDescription, character_show_description}) => {
+const GoTCharacters = (props) => {
+    const {characters,toggleCharacterDescription, syncCharacterEditData, character_show_description, character_edit_form_data,  show_character_edit, toggleEdit, editCharacterDetails} = props;
     return (
         <div className="characters-list">
             {characters.map(char => (
                 <CharacterRow 
                     character={char} 
                     character_show_description={character_show_description} 
+                    character_edit_form_data={character_edit_form_data} 
+                    show_character_edit={show_character_edit} 
                     toggleCharacterDescription={toggleCharacterDescription} 
+                    editCharacterDetails={editCharacterDetails} 
+                    syncCharacterEditData={syncCharacterEditData} 
+                    toggleEdit={toggleEdit} 
                     key={char.id}/>
             ))}
         </div>
@@ -17,16 +23,28 @@ const GoTCharacters = ({characters,toggleCharacterDescription, character_show_de
 
 export default GoTCharacters;
 
-export const CharacterRow = ({character, character_show_description, toggleCharacterDescription}) => (
-    <div className="row">
+export const CharacterRow = ({character, character_show_description, character_edit_form_data, show_character_edit, toggleCharacterDescription, toggleEdit, syncCharacterEditData, editCharacterDetails}) => {
+    const toggleEditPartial = toggleEdit.bind(null, character);
+    return (<div className="row">
         <div className="name">{character.name}</div>
         <a href="#" onClick={toggleCharacterDescription.bind(null, character)} >
             {character_show_description[character.id] ? 'collapse' : 'expand'}
         </a>
+        {!character_show_description[character.id] && <a href="#" onClick={toggleEditPartial} >
+            edit
+        </a>}
         {character_show_description[character.id] && 
             <div className="description">{character.description}</div>}
-    </div>
-);
+
+        {show_character_edit[character.id] &&
+            <EditCharacterDetails character={character} 
+                cancelEdit={toggleEditPartial} 
+                syncCharacterEditData={syncCharacterEditData} 
+                editCharacterDetails={editCharacterDetails} 
+                character_edit_form_data={character_edit_form_data}/>
+        }
+    </div>);
+}
 
 export class StatefulCharacterRow extends Component {
     constructor() {
@@ -48,4 +66,34 @@ export class StatefulCharacterRow extends Component {
                 <div className="description">{character.description}</div>}
         </div>);
     }
+};
+
+export const EditCharacterDetails = ({character, character_edit_form_data, syncCharacterEditData, editCharacterDetails, cancelEdit}) =>  {
+    const edit_data = character_edit_form_data[character.id];
+    const syncFormData = (key, e) => {
+        const {value} = e.currentTarget;
+        syncCharacterEditData(character, {
+            ...edit_data,
+            [key]: value
+        });
+    };
+
+    const saveForm = (e) => {
+        e.preventDefault();
+        editCharacterDetails(character);
+    };
+
+    return (
+        <form onSubmit={saveForm}> 
+            <label>Name: </label>
+            <input name='name' value={edit_data.name} onChange={syncFormData.bind(null, 'name')}/>
+            
+            <label>Description:</label>
+            <textarea name='description' value={edit_data.description} onChange={syncFormData.bind(null, 'description')}/>
+
+            <button type="reset" onClick={cancelEdit}> Cancel </button>
+            <button type="submit"> Submit </button>
+
+        </form>
+    );
 };
--- src/GoTCharactersContainer.js
+++ src/GoTCharactersContainer.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 import GoTCharacters from './GoTCharacters';
-import {toggleCharacterDescription} from './FlickDuck';
+import {toggleCharacterDescription, toggleEdit, syncCharacterEditData, editCharacterDetails} from './FlickDuck';
 
 const mapStateToProps = (state) => ({
     ...state.flick
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    toggleCharacterDescription : (data) => dispatch(toggleCharacterDescription(data))
+    toggleCharacterDescription : (data) => dispatch(toggleCharacterDescription(data)),
+    syncCharacterEditData : (character, form_data) => dispatch(syncCharacterEditData(character, form_data)),
+    editCharacterDetails : (character) => dispatch(editCharacterDetails(character)),
+    toggleEdit : (data) => dispatch(toggleEdit(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(GoTCharacters);
